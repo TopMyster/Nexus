fe3775b607c0cbd27894b8280e262035d20c45ca
TopMyster <topmyster@outlook.com>
2026-07-02 12:30:54 -0500

    feat: Github repo opens when the logo is pressed

--- src/App.tsx
+++ src/App.tsx
@@ -105,7 +105,7 @@ export default function App() {
       </AnimatePresence>
     ) : 
     <AnimatePresence>
-      <motion.h1 key={"logo"} initial={{ scale: 1.3 }} animate={{ scale: 1 }} className='logo'>Nexus</motion.h1>
+      <motion.h1 key={"logo"} initial={{ scale: 1.3 }} animate={{ scale: 1 }} className='logo' onClick={() => {window.open("https://github.com/TopMyster/Nexus","_blank")} }>Nexus</motion.h1>
       <motion.div key={"search-box"}  className='search-box' initial={{ scale: 0.7, opacity: 0 }} animate={{ scale: 1, opacity: 1 }}>
         <input key={"search-input"}  placeholder='Search anything...' value={searchQuery} onChange={(e) => {setSearchQuery(e.target.value)}} onKeyDown={(e) => {if (e.key === "Enter") {search(e.currentTarget.value)}}}/>
         <motion.button key={"Enter"}  initial={{ scale: 1, opacity: 1 }} whileTap={{ scale: 0.8, opacity: 0.8 }} onClick={() => {search(searchQuery)}}>
